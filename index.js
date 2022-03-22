75295c3670711462540a6848279e180f59451f50
Nadia Lut <3197344@gmail.com>
2022-03-22 20:05:48 +0200

    add js

---
+++ index.js
@@ -0,0 +1,35 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+const text = document.querySelector('h1')
+
+let intervalID = null;
+
+
+btnStart.addEventListener('click', changesBackgroundColor );
+btnStop.addEventListener('click', stopChangesBackgroundColorOfBody);
+
+
+function changesBackgroundColor() {
+  
+    intervalID = setInterval(() => {
+      text.style.color = `${getRandomHexColor()}`;
+    
+      // document.body.style.backgroundColor = `${getRandomHexColor()}`;
+      
+    
+    }, 1000);
+  btnStart.disabled = true;
+}
+
+function stopChangesBackgroundColorOfBody() {
+  clearInterval(intervalID);
+  console.log('хватит', btnStop); 
+  btnStart.disabled = false;
+}
+
+function getRandomHexColor() {
+return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+// text.style.color = `${getRandomHexColor()}`;
